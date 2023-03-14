a50135319e20be9830d7d7d9fc911f83352c345d
Arjun Sharma <arjun.sharma07@outlook.com>
2023-03-14 22:13:14 +0530

    Add tailwind and react config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()]
+});
