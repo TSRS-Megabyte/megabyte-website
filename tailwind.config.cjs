fe292eb76bea7316ec8cd53132bc0e073da429b9
Aarush Aggarwal <aggarwalaarushprogrammer@gmail.com>
2023-03-17 18:21:25 +0530

    added tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    mode: 'jit',
+    theme: {
+        colors: {
+            'blue': "82E8F",
+            'yellow': "#FFE81B",
+            'red': "#FF7474",
+            'green': "#CEFF1B"
+        },
+        extend: {},
+    },
+    plugins: [],
+}
