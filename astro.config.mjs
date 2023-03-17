682700423ed8b0afbc222001ede659749641a476
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-03-17 19:03:54 +0530

    removed random errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
