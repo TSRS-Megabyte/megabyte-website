908b3ed8066324eb88cd1abc5a0ad4110a2935cc
notskamr <66314478+notskamr@users.noreply.github.com>
2023-03-19 14:05:13 +0530

    Prettier-ified and switched to underlines with background image

--- navPages.js
+++ navPages.js
@@ -1,10 +1,10 @@
 // navPages lists all the pages on the navbar in the form 'Title':'PagePath' - must be in the order of display
 const navPages = {
-  "Events": "/events",
-  "Accomplishments": "/accomplishments",
-  "Resources": "/resources",
-  "About": "/about",
-}
-export default navPages
+  Events: "/events",
+  Accomplishments: "/accomplishments",
+  Resources: "/resources",
+  About: "/about",
+};
+export default navPages;
 
 // Edit this file to add or remove from the navbar.
