44aa28ca66d0d9e0e9a9f6a08bc14d6350988fb5
Aarush Aggarwal <aggarwalaarushprogrammer@gmail.com>
2023-03-22 20:14:41 +0530

    tried agan

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  mode: "jit",
-  theme: {
-    colors: {
-      bluetwo: "#82E8F",
-      yellow: "#FFE81B",
-      red: "#FF7474",
-      green: "#CEFF1B",
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    mode: "jit",
+    theme: {
+        colors: {
+            bluetwo: "#82E8F",
+            yellow: "#FFE81B",
+            red: "#FF7474",
+            green: "#CEFF1B",
+        },
+        extend: {
+
+        },
     },
-    extend: {
-      backgroundImage: {
-        "hero-pattern": "url('src/assets/anime-lines.svg')",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
