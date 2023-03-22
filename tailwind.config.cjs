e7db95a257a3067995d884a4262e40583d473fb4
Arjun Sharma <arjun.sharma07@outlook.com>
2023-03-22 21:21:20 +0530

    Fix nav on small screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     mode: "jit",
     theme: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px', // custom breakpoint
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+          },
         colors: {
             bluetwo: "#82E8F",
             yellow: "#FFE81B",
