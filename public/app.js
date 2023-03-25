8490fd3354761e5542eafa420ca699bd21396f03
Aarush Aggarwal <aggarwalaarushprogrammer@gmail.com>
2023-03-25 14:43:43 +0530

    resource slug

--- public/app.js
+++ public/app.js
@@ -2,6 +2,6 @@ import Swup from "swup";
 import SwupSlideTheme from "@swup/slide-theme";
 import SwupFadeTheme from "@swup/fade-theme";
 
-const swup = new Swup({
-  plugins: [new SwupFadeTheme(), new SwupSlideTheme()],
+export const swup = new Swup({
+    plugins: [new SwupFadeTheme(), new SwupSlideTheme()],
 });
