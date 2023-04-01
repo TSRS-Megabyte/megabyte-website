d78d32df796510d8d4c88aab2a82b8583c638e3e
Arjun Sharma <arjun.sharma07@outlook.com>
2023-04-01 23:17:48 +0530

    Remove notice board from nav

--- navPages.js
+++ navPages.js
@@ -1,8 +1,8 @@
 // navPages lists all the pages on the navbar in the form 'Title':'PagePath' - must be in the order of display
 const navPages = {
-    "Notice Board": "/notice-board",
+    // "Notice Board": "/notice-board",
     "Accomplishments": "/accomplishments",
-    // "Resources": "/resources",
+    "Resources": "/resources",
     "About": "/about",
 };
 export default navPages;
