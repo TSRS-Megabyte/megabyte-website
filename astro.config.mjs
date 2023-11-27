a06f3abcd58f876a3596697a2142d3f3670471cd
Varun Sahni <sahnivarun62@gmail.com>
2023-11-27 13:17:52 +0530

    Event listener for the text scrambling so it doesn't just disappear when renavigating

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tsrsmegabyte.com",
+  trailingSlash: "never",
   prefetch: {
     prefetchAll: true,
   },
