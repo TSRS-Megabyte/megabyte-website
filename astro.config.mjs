81c860e639dad93ab5967400824d2467aceeb647
Varun Sahni <sahnivarun62@gmail.com>
2023-11-27 13:30:20 +0530

    Needed to add a / for unrelative route

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tsrsmegabyte.com",
-  trailingSlash: "never",
   prefetch: {
     prefetchAll: true,
   },
